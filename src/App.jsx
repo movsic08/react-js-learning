b79e9234b02d00ac9be17ceb27a681fcfef57e02
movsic08 <lejero08@gmail.com>
2024-05-30 16:49:00 +0800

    learned useEffect and localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [number, setNumber] = useState(Number(localStorage.getItem("number")));
+  useEffect(() => {
+    localStorage.setItem("number", number);
+  }, [number]);
 
   return (
     <>
       <div>
         <h2 className=" text-slate-900 p-4 bg-blue-600 rounded-lg">HEllo</h2>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setNumber((number) => number + 3)}>
+          {" "}
+          your number is {number}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "selector",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
